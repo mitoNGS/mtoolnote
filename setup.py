ca550f11c90c80ec5f3774587115fbb364fe315c
Roberto Preste <roberto.preste@gmail.com>
2019-11-30 10:58:10 +0000

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(  # pragma: no cover
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/mitoNGS/mtoolnote",
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=False,
 )
